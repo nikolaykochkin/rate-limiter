5472863b127da86f55372ca027b6dddb30857e98
nikolaykochkin <nikolaykochkin91@gmail.com>
2023-11-10 10:53:30 +0100

    Initial commit

---
+++ src/test/java/name/nikolaikochkin/ratelimiter/RateLimiterApplicationTests.java
@@ -0,0 +1,13 @@
+package name.nikolaikochkin.ratelimiter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RateLimiterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
